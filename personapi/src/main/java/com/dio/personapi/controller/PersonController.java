6cb122db6bfc4498da112729242afdfed2efda1d
flai19 <flaioliver@hotmail.com>
2021-07-28 22:24:31 -0300

    first commit

--- personapi/src/main/java/com/dio/personapi/controller/PersonController.java
+++ personapi/src/main/java/com/dio/personapi/controller/PersonController.java
@@ -1,4 +1,4 @@
-package com.dio.personapi.mapper;
+package com.dio.personapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,6 @@ public class PersonController {
 
     @GetMapping
     public String getBook(){
-        return "API Testi";
+        return "API Test";
     }
 }
